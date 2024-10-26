39d024ff1005ae9ec7499c76d66ac1d5d9daff9d
GDB <eqeq0824@gmail.com>
2024-10-26 17:07:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -15,7 +15,7 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> N >> K;
-	visited.resize(200001, INT_MAX);
+	visited.resize(100001, INT_MAX);
 
 	BFS();
 	cout << visited[K];
@@ -28,15 +28,15 @@ void BFS() {
 	while (visited[K] == INT_MAX) {
 		int now = q.front();
 		q.pop();
-		if (now + 1 < K * 2 && visited[now + 1] == INT_MAX) {
+		if (now + 1 < visited.size() && visited[now + 1] == INT_MAX) {
 			visited[now + 1] = visited[now] + 1;
 			q.push(now + 1);
 		}
-		if (now - 1 >= 0 && visited[now - 1] == INT_MAX) {
+		if (now > 0 && visited[now - 1] == INT_MAX) {
 			visited[now - 1] = visited[now] + 1;
 			q.push(now - 1);
 		}
-		if (now < K && visited[now * 2] == INT_MAX) {
+		if (now * 2 < visited.size() && visited[now * 2] == INT_MAX) {
 			visited[now * 2] = visited[now] + 1;
 			q.push(now * 2);
 		}
