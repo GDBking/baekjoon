b681d43c4ade396b3b361562e5df9abfd1cad337
GDB <eqeq0824@gmail.com>
2024-10-26 13:10:59 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,21 +1,19 @@
 #include <iostream>
 using namespace std;
 
-static int n, D[1001];
+int DP[1001];
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int n;
 	cin >> n;
-	D[1] = 1;
-	D[2] = 2;
+	DP[1] = 1;
+	DP[2] = 2;
 	for (int i = 3; i <= n; i++)
-		D[i] = (D[i - 2] + D[i - 1]) % 10007;
+		DP[i] = (DP[i - 2] + DP[i - 1]) % 10007;
 
-	cout << D[n];
-
-	return 0;
+	cout << DP[n];
 }
