b8a42d272a66e80dc0e44c435cb709e861593c0b
GDB <eqeq0824@gmail.com>
2024-10-27 21:12:06 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<char> st;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	char buffer[101] = " ";
+	while (true) {
+		st = {};
+		cin.getline(buffer, 101, '.');
+		cin.ignore();
+		if (!*buffer) break;
+		char* p = buffer;
+		while (*p) {
+			if (*p == '(')
+				st.push('(');
+			else if (*p == '[')
+				st.push('[');
+			else if (*p == ')') {
+				if (st.empty() || st.top() != '(') {
+					st.push('x');
+					break;
+				}
+				st.pop();
+			}
+			else if (*p == ']') {
+				if (st.empty() || st.top() != '[') {
+					st.push('x');
+					break;
+				}
+				st.pop();
+			}
+			p++;
+		}
+		if (st.empty())
+			cout << "yes\n";
+		else
+			cout << "no\n";
+	}
+}
