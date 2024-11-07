b11dd4550524e8f0d609a079e579d5183914320d
GDB <eqeq0824@gmail.com>
2024-11-07 18:00:48 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -19,9 +19,9 @@ int main() {
 
     dp[0] = wine[0];
     dp[1] = wine[0] + wine[1];
-    dp[2] = max(max(wine[0] + wine[2], wine[1] + wine[2]), dp[1]);
+    dp[2] = max(max(wine[0], wine[1]) + wine[2], dp[1]);
     for (int i = 3; i < n; i++)
-        dp[i] = max(max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]), dp[i - 1]);
+        dp[i] = max(max(dp[i - 2], dp[i - 3] + wine[i - 1]) + wine[i], dp[i - 1]);
 
     cout << dp[n - 1];
 }
